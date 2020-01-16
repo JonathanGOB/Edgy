9e7a6eab06178511edf62cfe5af402b8adaa3cca
Faimire <faimireg@hotmail.com>
2020-01-16 23:17:20 +0100

    small changes

--- iotedge-api/InsertData.py
+++ iotedge-api/InsertData.py
@@ -3,15 +3,15 @@ from TableStorageConnection import AzureTableStorage
 table_service = AzureTableStorage().get_table()
 
 # Insert a new entity
-task = {'PartitionKey': 'Groningen', 'RowKey': '004',
+task = {'PartitionKey': 'Groningen', 'RowKey': '0',
         'description': 'Buy detergent'}
 
 # Insert a new entity
-task1 = {'PartitionKey': 'Groningen', 'RowKey': '005',
+task1 = {'PartitionKey': 'Groningen', 'RowKey': '1',
         'description': 'Buy detergent'}
 
 # Insert a new entity
-task2 = {'PartitionKey': 'Groningen', 'RowKey': '006',
+task2 = {'PartitionKey': 'Groningen', 'RowKey': '2',
         'description': 'Buy detergent'}
 
 with table_service.batch('users') as batch:
--- iotedge-api/User.py
+++ iotedge-api/User.py
@@ -2,13 +2,12 @@ from flask_restful import Api, Resource, reqparse, fields, marshal
 from datetime import datetime
 from TableStorageConnection import AzureTableStorage
 
-deviceData_fields = {
+User_fields = {
     'PartitionKey': fields.String,
     'RowKey': fields.Integer,
     'Name': fields.String,
     'Email': fields.String,
     'Password': fields.String,
-    'timestamp': fields.DateTime,
     'uri': fields.Url('User')
 }
 
@@ -20,15 +19,20 @@ class User(Resource):
         self.reqparse.add_argument('Name', type=str, required=False)
         self.reqparse.add_argument('Email', type=str, required=False)
         self.reqparse.add_argument('Password', type=str, required=False)
-        self.reqparse.add_argument('timestamp', type=lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'),
-                                   required=False)
+        self.reqparse.add_argument('Password', type=str, required=False)
+        self.reqparser.add_argument('Authorization', type=str, location='headers', required=False)
         super(User, self).__init__()
 
     def get(self):
+        args = self.reqparse.parse_args()
+        filter = "Name eq '{}'".format(args['Name'])
+        
+
+    def post(self):
         storage = AzureTableStorage()
         table_service = storage.get_table()
-        filter = "PartitionKey eq 'Groningen'and Timestamp lt datetime'{}'".format(datetime.utcnow().isoformat())
-        print(filter)
-        rows = table_service.query_entities('users', filter=filter)
-        for row in rows:
-            print(row)
+        rows = table_service.query_entities('users')
+        rows = list(rows)
+        latest_user = rows[len(rows) - 1]
+        NewRowKey = int(latest_user['RowKey']) + 1
+
