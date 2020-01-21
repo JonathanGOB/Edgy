0e29c33e973f3062bfd71199267768d2ee301349
Faimire <faimireg@hotmail.com>
2020-01-21 16:44:17 +0100

    order shift

--- SensorDataInserter/main.py
+++ SensorDataInserter/main.py
@@ -7,7 +7,7 @@ def loop():
     sensordataqueue = table_service.query_entities('sensordataqueue')
     for sensordata in sensordataqueue:
         sensordata_table = None
-        
+
         try:
             filter = "PartitionKey eq 'sensordata'"
             sensordata_table = table_service.query_entities('rulers', filter=filter)
@@ -16,21 +16,18 @@ def loop():
                                 "RowKey": sensordata_table['RowKey'],
                                 "NewId": sensordata_table["NewId"] + 1, "Size": sensordata_table["Size"] + 1}
             table_service.update_entity('rulers', ruler_sensordata, if_match=sensordata_table["etag"])
-            isNew = True
+            try:
+                sensors_fields = {
+                    "PartitionKey": sensordata["PartitionKey"].replace("'", ";"),
+                    "RowKey": str(sensordata_table["NewId"]),
+                    "Datavalue": sensordata["Datavalue"]
+                }
+
+                table_service.insert_entity('sensordata', sensors_fields)
+            except Exception as e:
+                print("error: ", e)
         except:
             print("concurrency problems")
-
-
-        try:
-            sensors_fields = {
-                "PartitionKey": sensordata["PartitionKey"].replace("'", ";"),
-                "RowKey": str(sensordata_table["NewId"]),
-                "Datavalue": sensordata["Datavalue"]
-            }
-
-            table_service.insert_entity('sensordata', sensors_fields)
-        except Exception as e:
-            print("error: ", e)
 
 
 if __name__ == '__main__':
