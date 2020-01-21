268799cd38401503a31a6fbbde883dc3a6b1b366
Faimire <faimireg@hotmail.com>
2020-01-21 21:52:56 +0100

    jwt

--- iotplatform/src/store/index.js
+++ iotplatform/src/store/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import User from "../models/User";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: null,
+    user: null,
+  },
+  getters: {
+    user: (state) => state.user,
   },
   mutations: {
   },
--- iotplatform/src/views/Home.vue
+++ iotplatform/src/views/Home.vue
@@ -12,7 +12,7 @@ import store from '../store'
 export default {
   name: 'home',
   components: {
-    
+
   }
 }
 </script>
