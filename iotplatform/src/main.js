86dd6c96d345e089f9c82b40f172c1b7d8ba5072
Faimire <faimireg@hotmail.com>
2020-01-21 21:42:36 +0100

    morrow

--- iotplatform/src/main.js
+++ iotplatform/src/main.js
@@ -6,12 +6,16 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VeeValidate from 'vee-validate';
+
 
 Vue.config.productionTip = false
 // Install BootstrapVue
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(VeeValidate);
 
 new Vue({
   router,
