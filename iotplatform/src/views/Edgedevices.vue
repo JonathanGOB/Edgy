0aa2c77e4ae58e70f1d5f25b483ac33c744d5bbd
Faimire <faimireg@hotmail.com>
2020-01-23 22:36:13 +0100

    example

--- iotplatform/src/views/Edgedevices.vue
+++ iotplatform/src/views/Edgedevices.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="home">
+
         <Sidebar>
         </Sidebar>
+        <div class="page-content p-5">
+            <EdgeDevicesTable>
+            </EdgeDevicesTable>
+        </div>
     </div>
 </template>
 
 <script>
     import Sidebar from "../components/Sidebar";
+    import EdgeDevicesTable from "../components/EdgeDevicesTable";
 
     export default {
         name: "Edgedevices",
         components: {
-            Sidebar
-
+            Sidebar,
+            EdgeDevicesTable
         }
     }
 </script>
 
 <style scoped>
-
+    .page-content {
+        width: calc(100% - 11rem);
+        margin-left: 14rem;
+        transition: all 0.4s;
+    }
 </style>
