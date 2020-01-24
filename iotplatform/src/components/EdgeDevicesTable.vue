0436b2e53dfc455b6b77f39f3ec31de755279df2
Faimire <faimireg@hotmail.com>
2020-01-24 16:07:55 +0100

    bar

--- iotplatform/src/components/EdgeDevicesTable.vue
+++ iotplatform/src/components/EdgeDevicesTable.vue
@@ -1,6 +1,51 @@
 <template>
     <div>
-        <b-table striped hover :items="items"></b-table>
+        <b-container>
+        <b-row class="d-flex flex-column-reverse align-items-center">
+            <h1 class="display-3"> Edgedevices </h1>
+        </b-row>
+            <b-row>
+                <b-button v-b-modal.modal-1>Create Edge device</b-button>
+                <b-modal id="modal-1" title="BootstrapVue">
+                    <form @submit.prevent>
+                        <div class="form-group">
+                            <label class="mb-0" for="inputEmail">Email</label>
+                            <input
+                                    type="email"
+                                    id="inputEmail"
+                                    class="form-control"
+                                    required
+                                    autofocus
+                                    v-model="email"
+                            >
+                        </div>
+
+                        <div class="form-group">
+                            <label class="mb-0 text-dark" for="inputPassword">Password</label>
+                            <input
+                                    type="password"
+                                    id="inputPassword"
+                                    class="form-control"
+                                    v-model="password"
+                                    minlength="8"
+                                    required
+                            >
+                        </div>
+
+                        <button type="button" class="btn btn-md btn-primary btn-block mb-3 btn-dark" @click="login">
+                            Sign in
+                            <font-awesome-icon icon="spinner" v-if="loading" spin/>
+                        </button>
+
+                        <div>
+                            <span style="color:red">{{ error }}</span>
+                        </div>
+                    </form>
+                </b-modal>
+            </b-row>
+            <b-table striped hover :items="items"></b-table>
+        </b-container>
+
     </div>
 </template>
 
--- iotplatform/src/components/Sidebar.vue
+++ iotplatform/src/components/Sidebar.vue
@@ -1,41 +1,22 @@
 <template>
-    <div>
-        <div class="vertical-nav bg-white" id="sidebar">
-            <div class="py-4 px-3 mb-4 bg-light">
-                <div class="media d-flex align-items-center"><img
-                        src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/54424687_1498788586918848_5011177178784595968_n.jpg?_nc_cat=102&_nc_ohc=ZkuHu6xLYW4AX_0o92d&_nc_ht=scontent-frt3-1.xx&oh=842ee679d4bf42e80ec5ac4fd347b728&oe=5ECE75CC"
-                        alt="..."
-                        width="65" class="mr-3 rounded-circle img-thumbnail shadow-sm">
-                    <div class="media-body" v-if="this.$store.getters.user">
-                        <h4 class="m-0">{{this.$store.getters.user.name}}</h4>
-                        <p class="font-weight-light text-muted mb-0">{{this.$store.getters.user.email}}</p>
-                    </div>
-                </div>
+    <b-navbar type="light" variant="light">
+        <b-navbar-brand>Edgy</b-navbar-brand>
+        <b-navbar-nav>
+            <div v-for="item in items.devices" v-bind:key="item.title">
+                <b-nav-item :to="{ path: item.link }">{{item.title}}</b-nav-item>
             </div>
-
-            <p class="text-gray font-weight-bold text-uppercase px-3 small pb-4 mb-0">Account</p>
-
-            <ul class="nav flex-column bg-white mb-0">
-                <li v-for="item in items.user" v-bind:key="item.title" class="nav-item">
-                    <router-link class="nav-link text-dark font-italic" :to="{name: item.link}">
-                        <v-icon>{{item.icon}}</v-icon>
-                        {{item.title}}
-                    </router-link>
-                </li>
-            </ul>
-
-            <p class="text-gray font-weight-bold text-uppercase px-3 small py-4 mb-0">Devices</p>
-
-            <ul class="nav flex-column bg-white mb-0">
-                <li v-for="item in items.devices" v-bind:key="item.title" class="nav-item">
-                    <router-link class="nav-link text-dark font-italic" :to="{name: item.link}">
-                        <v-icon>{{item.icon}}</v-icon>
-                        {{item.title}}
-                    </router-link>
-                </li>
-            </ul>
+        </b-navbar-nav>
+        <div class="ml-auto">
+        <b-img v-bind="mainProps" src="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/54424687_1498788586918848_5011177178784595968_n.jpg?_nc_cat=102&_nc_ohc=ZkuHu6xLYW4AX_0o92d&_nc_ht=scontent-frt3-1.xx&oh=842ee679d4bf42e80ec5ac4fd347b728&oe=5ECE75CC" rounded alt="Rounded image" right></b-img>
+        <b-navbar-nav>
+        <b-nav-item-dropdown v-bind:text=this.$store.getters.user.name>
+                <div v-for="item in items.user" v-bind:key="item.title">
+                    <b-dropdown-item :to="{ path: item.link }">{{item.title}}</b-dropdown-item>
+                </div>
+        </b-nav-item-dropdown>
+        </b-navbar-nav>
         </div>
-    </div>
+    </b-navbar>
 </template>
 
 <script>
@@ -54,20 +35,21 @@
         },
         data() {
             return {
-                eslint: "",
                 items: {
                     devices: [
-                        {title: 'EdgeDevices', icon: 'mdi-monitor', link: 'edgedevices',},
-                        {title: 'Sensorsdevices', icon: 'mdi-minus-network', link: 'about'},
-                        {title: 'Sensors', icon: 'mdi-satellite-variant', link: 'about'},
-                        {title: 'Sensordata', icon: 'mdi-floppy', link: 'about'},
-                        {title: 'Logout', icon: 'mdi-account-key', link: 'logout'}
+                        {title: 'Dashboard', icon: 'mdi-view-dashboard', link: '/'},
+                        {title: 'EdgeDevices', icon: 'mdi-monitor', link: '/edgedevices',},
+                        {title: 'Sensorsdevices', icon: 'mdi-minus-network', link: '/about'},
+                        {title: 'Sensors', icon: 'mdi-satellite-variant', link: '/about'},
+                        {title: 'Sensordata', icon: 'mdi-floppy', link: '/about'},
+
                     ],
                     user: [
-                        {title: 'Dashboard', icon: 'mdi-view-dashboard', link: 'home'},
-                        {title: 'Account', icon: 'mdi-account-box', link: 'about'}
+                        {title: 'Account', icon: 'mdi-account-box', link: '/about'},
+                        {title: 'Logout', icon: 'mdi-account-key', link: '/logout'}
                     ]
                 },
+                mainProps: {width: 35, height: 35, class: 'm1' },
                 created: false,
                 computed: {}
                 ,
@@ -92,7 +74,6 @@
         position: fixed;
         top: 0;
         left: 0;
-        box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.1);
         transition: all 0.4s;
     }
 
