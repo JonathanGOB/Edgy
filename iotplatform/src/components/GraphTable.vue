a11d2e6c0ecc0e83b53f25b25e6b78c932a0535f
Faimire <faimireg@hotmail.com>
2020-01-25 21:45:17 +0100

    Starter graph

---
+++ iotplatform/src/components/GraphTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GraphTable"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- iotplatform/src/views/SpecifickSensordata.vue
+++ iotplatform/src/views/SpecifickSensordata.vue
@@ -3,14 +3,36 @@
         <Sidebar>
 
         </Sidebar>
+        <b-container fluid>
+            <b-row>
+                <Graph v-bind:data="data">
+
+                </Graph>
+            </b-row>
+            <b-row>
+                <GraphTable v-bind:data="data">
+
+                </GraphTable>
+            </b-row>
+        </b-container>
     </div>
 </template>
 
 <script>
     import Sidebar from "../components/Sidebar";
+    import Graph from "../components/Graph";
+    import GraphTable from "../components/GraphTable";
     export default {
         name: "SpecifickSensordata",
-        components: {Sidebar}
+        components: {GraphTable, Graph, Sidebar},
+        data(){
+            return{
+                data: []
+            }
+        },
+        created() {
+
+        }
     }
 </script>
 
